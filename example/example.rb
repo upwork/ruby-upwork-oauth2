3304614e9c9ca83ec74a4fdec7f8424a3eba834b
Maksym Novozhylov <mnovozhilov@cloud.upwork.com>
2024-11-27 17:03:48 +0100

    chore: fix examples

--- example/example.rb
+++ example/example.rb
@@ -6,10 +6,6 @@ $LOAD_PATH << File.dirname(__FILE__)
 
 require 'upwork/api'
 require 'upwork/api/routers/graphql'
-require 'upwork/api/routers/auth'
-require 'upwork/api/routers/messages'
-require 'upwork/api/routers/reports/time'
-require 'upwork/api/routers/freelancers/search'
 
 # initiate config
 config = Upwork::Api::Config.new({
@@ -52,40 +48,19 @@ end
 @actual_access_token_data = client.get_actual_config
 
 # execute graphql request
-#client.set_org_uid_header('1234567890') # Organization UID (optional)
-#graphql = Upwork::Api::Routers::Graphql.new(client)
-#params = {
-#  'query' => "query {
-#      user {
-#        id
-#        nid
-#        rid
-#      }
-#      organization {
-#        id
-#      }
-#    }"
-#}
-#data = graphql.execute params
-#p data['data']['user']['id']
-
-# get my auth data
-#auth = Upwork::Api::Routers::Auth.new(client)
-#info = auth.get_user_info
-#p info['server_time']
-
-# work with timereports
-#report = Upwork::Api::Routers::Reports::Time.new(client)
-#report_response = report.get_by_freelancer_limited('xxxxxxxx', {'tqx' => 'out:json', 'tq' => "select task where worked_on >= '2018-01-01' AND worked_on <= '2018-01-10' order by worked_on"})
-
-# work with search
-#params = {'q' => 'python AND java AND (perl OR php)'}
-#freelancers = Upwork::Api::Routers::Freelancers::Search.new(client)
-#p freelancers.find(params)
-
-# work with messages
-#messages = Upwork::Api::Routers::Messages.new(client)
-#params = {
-#  'story' => '{"message": "test message from api", "userId": "~xxxxxxxx", "orgId": "xxxxxxxx"}'
-#}
-#messages.send_message_to_room 'company-xxxxxxxx', 'room-xxxxxxxx', params
+client.set_org_uid_header('1234567890') # Organization UID (optional)
+graphql = Upwork::Api::Routers::Graphql.new(client)
+params = {
+  'query' => "query {
+      user {
+        id
+        nid
+        rid
+      }
+      organization {
+        id
+      }
+    }"
+}
+data = graphql.execute params
+p data['data']['user']['id']
